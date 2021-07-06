529b245e3be812e1a87cd40b283dbbf10adf763c
Filippo Balestrini <54440701+fbalestrini@users.noreply.github.com>
2021-07-06 23:02:14 +0200

    Update mgos_bvar_json.c

--- src/mgos_bvar_json.c
+++ src/mgos_bvar_json.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 #include "mgos_bvar_json.h"
 
+#ifdef MGOS_BVAR_HAVE_JSON
+
 bool mgos_bvar_json_try_scanf(const char *payload, mgos_bvar_t *out_var) {
   if (out_var == NULL) return false;
   *out_var = mgos_bvar_json_scanf(payload);
@@ -14,6 +16,8 @@ bool mgos_bvar_json_try_bscanf(const char *payload, int payload_len, mgos_bvar_t
   *out_var = mgos_bvar_json_bscanf(payload, payload_len);
   return !mgos_bvar_is_null(*out_var);
 }
+
+#endif // MGOS_BVAR_HAVE_JSON
 
 bool mgos_bvar_json_init() {
   return true;
